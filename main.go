0ff79bddfa28118c7cc8680df3f32ea0ce03a7d3
John Spence <johnspence0212@gmail.com>
2024-08-10 11:03:41 -0500

    Using the bubbletea list now

--- main.go
+++ main.go
@@ -5,14 +5,55 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
+	"strings"
 
 	"github.com/charmbracelet/bubbles/list"
 	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 )
 
 var globalList []string
+
+const listHeight = 14
+
+var (
+	titleStyle        = lipgloss.NewStyle().MarginLeft(2)
+	itemStyle         = lipgloss.NewStyle().PaddingLeft(4)
+	selectedItemStyle = lipgloss.NewStyle().PaddingLeft(2).Foreground(lipgloss.Color("170"))
+	paginationStyle   = list.DefaultStyles().PaginationStyle.PaddingLeft(4)
+	helpStyle         = list.DefaultStyles().HelpStyle.PaddingLeft(4).PaddingBottom(1)
+	quitTextStyle     = lipgloss.NewStyle().Margin(1, 0, 2, 4)
+)
+
+type item string
+
+func (i item) FilterValue() string { return "" }
+
+type itemDelegate struct{}
+
+func (d itemDelegate) Height() int                             { return 1 }
+func (d itemDelegate) Spacing() int                            { return 0 }
+func (d itemDelegate) Update(_ tea.Msg, _ *list.Model) tea.Cmd { return nil }
+func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list.Item) {
+	i, ok := listItem.(item)
+	if !ok {
+		return
+	}
+
+	str := fmt.Sprintf("%d. %s", index+1, i)
+
+	fn := itemStyle.Render
+	if index == m.Index() {
+		fn = func(s ...string) string {
+			return selectedItemStyle.Render("> " + strings.Join(s, " "))
+		}
+	}
+
+	fmt.Fprint(w, fn(str))
+}
 
 func main() {
 	p := tea.NewProgram(initialModel(), tea.WithAltScreen())
@@ -39,7 +80,20 @@ func initialModel() model {
 	ti.CharLimit = 156
 	ti.Width = 20
 
+	items := []list.Item{}
+
+	const defaultWidth = 20
+
+	l := list.New(items, itemDelegate{}, defaultWidth, listHeight)
+	l.Title = "What do you want for dinner?"
+	l.SetShowStatusBar(false)
+	l.SetFilteringEnabled(false)
+	l.Styles.Title = titleStyle
+	l.Styles.PaginationStyle = paginationStyle
+	l.Styles.HelpStyle = helpStyle
+
 	return model{
+		list:      l,
 		textInput: ti,
 		err:       nil,
 	}
@@ -56,11 +110,11 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	case tea.KeyMsg:
 		switch msg.Type {
 		case tea.KeyCtrlC, tea.KeyEsc:
-			return nil, tea.Quit
+			return m, tea.Quit
 		case tea.KeyEnter:
 			m.input = m.textInput.Value()
 			taskFormatted := fmt.Sprintf("[ ] %s", m.input)
-			globalList = append(globalList, taskFormatted)
+			m.list.InsertItem(99999, item(taskFormatted))
 			m.textInput.SetValue("")
 		}
 
@@ -80,7 +134,7 @@ func (m model) View() string {
 		listItems += fmt.Sprintf("%d %s\n", i, item)
 	}
 
-	return listItems + "\n" + fmt.Sprintf(
+	return m.list.View() + "\n" + fmt.Sprintf(
 		"What’s your favorite Pokémon?\n\n%s\n\n%s",
 		m.textInput.View(),
 		"(esc to quit)",
